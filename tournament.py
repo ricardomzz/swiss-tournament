30789c5bcd48ca4658f7af0c83c5af52056e78eb
Ricardo Mazzini <ricardomzz@gmail.com>
2017-03-23 12:13:31 -0400

    modified:   tournament.py implemented: reportMatch()

--- tournament.py
+++ tournament.py
@@ -81,6 +81,12 @@ def reportMatch(winner, loser):
       loser:  the id number of the player who lost
     """
 
+    conn = connect()
+    c = conn.cursor()
+    c.execute("INSERT INTO matches (winner,loser) VALUES (%s,%s);", (winner,loser,))
+    conn.commit()
+    conn.close()
+    return
 
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
