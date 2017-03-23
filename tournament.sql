6ff5417b651cd09275c942265c2c4db299429e88
Ricardo Mazzini <ricardomzz@gmail.com>
2017-03-23 11:49:31 -0400

    modified:   tournament.sql Created players and matches tables

--- tournament.sql
+++ tournament.sql
@@ -6,4 +6,18 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS matches;
+DROP TABLE IF EXISTS standings;
+DROP TABLE IF EXISTS pairings;
 
+CREATE TABLE players(
+  id serial PRIMARY KEY,
+  name text
+);
+
+CREATE TABLE matches(
+  id serial PRIMARY KEY,
+  winner integer references players(id),
+  loser integer references players(id)
+);
