d7f7bc174c63091cf3321881d1d5a8574d866db5
Ricardo Mazzini <ricardomzz@gmail.com>
2017-03-23 17:37:47 -0400

    modified:   tournament.sql formatting

--- tournament.sql
+++ tournament.sql
@@ -5,13 +5,8 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
-
-DROP TABLE IF EXISTS players;
-DROP TABLE IF EXISTS matches;
-DROP TABLE IF EXISTS standings;
-DROP TABLE IF EXISTS pairings;
-DROP VIEW IF EXISTS wins;
-DROP VIEW IF EXISTS standings;
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
 
 CREATE TABLE players(
   id serial PRIMARY KEY,
@@ -25,12 +20,20 @@ CREATE TABLE matches(
 );
 
 
-CREATE VIEW wins AS SELECT winner as id, count(winner) from matches group by winner;
+CREATE VIEW wins AS
+SELECT winner as id, count(winner)
+from matches
+group by winner;
 
-CREATE VIEW losses AS SELECT loser as id, count(loser) from matches group by loser;
+CREATE VIEW losses AS
+SELECT loser as id, count(loser)
+from matches
+group by loser;
 
 --The standings view uses coalesce to ensure a value is always present
 --if a player has no matche or no wins the expression coalesces to 0
-CREATE VIEW standings as SELECT players.id, players.name,coalesce(wins.count,0)
-AS wins, coalesce(wins.count+losses.count,0) AS matches FROM players LEFT JOIN
-wins ON players.id = wins.id LEFT JOIN losses ON players.id = losses.id;
+CREATE VIEW standings AS
+SELECT players.id, players.name,coalesce(wins.count,0) AS wins,
+coalesce(wins.count,0)+coalesce(losses.count,0) AS matches
+FROM players LEFT JOIN wins ON
+ players.id = wins.id LEFT JOIN losses ON players.id = losses.id;
